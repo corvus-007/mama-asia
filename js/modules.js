407aba9bdc6de0336ec5dd63306d813271105c3c
Denis <corvus007@yandex.ru>
2018-11-02 20:17:13 +0300

    Update 2018-11-02T17:16:59.377Z

--- js/modules.js
+++ js/modules.js
@@ -59,6 +59,30 @@ window.splashScreen = (function (window, $) {
   });
 })(window, jQuery);
 
+window.offerPopup = (function (window, $) {
+  'use strict';
+
+  var isShowedOfferPopup = Cookies.get('offerPopup');
+  var OFFER_POPUP_OFFSET_TIME = 10000;
+  var phoneOfferPopupTimeout = setTimeout(function () {
+    $.fancybox.open({
+      src: '#offer-popup',
+      type: 'inline',
+      opts: {
+        afterClose: function () {
+          Cookies.set('offerPopup', 1, {
+            expires: 30
+          });
+        }
+      }
+    });
+  }, OFFER_POPUP_OFFSET_TIME);
+
+  if (isShowedOfferPopup) {
+    clearTimeout(phoneOfferPopupTimeout);
+  }
+})(window, jQuery);
+
 window.searchBox = (function () {
   'use strict';
 
