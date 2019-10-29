ca22e360b47ef0c28fd1a74dbf8d284c83b69f00
Denis <corvus007@yandex.ru>
2019-10-29 09:49:28 +0300

    Скрывает поиск

--- app/js/modules.js
+++ app/js/modules.js
@@ -2,7 +2,7 @@
 //=require modules/splash-screen.js
 //=require modules/mobile-menu.js
 //=require modules/offer-popup.js
-//=require modules/search-box.js
+////=require modules/search-box.js
 //=require modules/map.js
 //=require modules/delivery-map.js
 //=require modules/out-cover.js
